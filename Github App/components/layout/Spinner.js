3b518ef9c6fe709307c4b7ff607686f351fda62b
Guido Valenzuela <nucleico@gmail.com>
2020-07-31 14:29:29 -0300

    github

---
+++ Github App/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Spinner = (props) => {
+    return (
+        <Fragment>
+            <h3 style={{color: "blue", fontWeight: "bold"}}>LOADING...</h3>            
+        </Fragment>
+    )
+}
+
+export default Spinner
